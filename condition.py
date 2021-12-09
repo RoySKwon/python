07249a211a944c88e0b927937622add9ab43623b
RoySKwon <roykwonfirmen@gmail.com>
2021-12-09 16:36:23 +0100

    iterator and condition of python

---
+++ condition.py
@@ -0,0 +1,29 @@
+# condition
+
+if True or False:
+    print("YES");
+else:
+    print("NO");
+
+a = 15;
+
+if a >= 0 and a <= 20:
+    print("YES");
+if 1 < a < 16:
+    pass # print("YES");
+else:
+    print("NO");
+
+score = 85;
+
+# if score >= 80: result = "Success" 
+if score >= 80: 
+    result = "Success" 
+print(result);
+
+score = 90;
+# Conditional Expression
+result = "Fale" if score < 90 else "Sucdess";
+print(result);
+
+    
--- iterater.py
+++ iterater.py
@@ -1,1 +1,65 @@
-iterater
+# iterater
+
+i = 1;
+result = 0;
+
+while i <= 10:
+    if i % 2 == 1:
+        result += i;
+    i += 1;
+    print(result);
+print(type(result), result); 
+
+arrayList = [9, 8, 7, 6, 5];
+
+for x in arrayList:
+    print(x);
+
+arrayTuple = (1, 2, 3, 4, 5);
+
+for x in  arrayTuple:
+    print(x);
+
+result = 0;
+for x in range(10):
+    result += x;
+    print(result);
+
+result = 0;
+for x in range(1, 101):    
+    result += x;
+    print(f"x {x} result {result}" )
+print(result);    
+
+result = 0;
+
+for x in range(1, 11):
+    if x % 2 == 0:
+        print("continue", x);
+        continue;
+    result += x;
+print(result);
+
+i = 1;
+while True:
+    print(i);
+    if i >= 5 :
+        break;    
+    i += i;
+print("exit: ", i)
+
+scores = [ 100, 95, 45, 75, 65, 50, 80, 90, 100, 77];
+cheating_student_list = {0, 8}
+
+for x in range(10):
+    if x in cheating_student_list:
+        continue;
+    if scores[x] >= 60:
+        print(scores[x], "Success");
+    else:
+        print(scores[x], "FAIL");    
+
+for i in range(2, 10):
+    for j in range(1, 10):
+        print(i, "X", j, '=', i * j);
+    print("");    
