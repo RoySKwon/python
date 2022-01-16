1ab32167649073b55e8f9828a628f0adb0133a57
RoySKwon <roykwonfirmen@gmail.com>
2022-01-16 17:03:36 +0100

    stack

---
+++ stack01.py
@@ -0,0 +1,33 @@
+# stack01.py
+#FILO
+
+stack = [];
+print(stack);
+
+#push
+stack.append(5);
+print(stack);
+
+stack.append(2);
+print(stack);
+
+stack.append(3);
+print(stack);
+
+stack.append(7);
+print(stack);
+
+#pop
+stack.pop();
+print(stack);
+
+#push
+stack.append(1)
+print(stack);
+
+stack.append(4);
+print(stack);
+
+#pop
+stack.pop();
+print(stack);
